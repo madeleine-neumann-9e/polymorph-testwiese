86cf8a5b1855cf10ea0adbac5a15838d34325472
Madeleine Neumann <madeleine.neumann@9elements.com>
2021-02-10 11:14:11 +0100

    wip

--- app/models/button.rb
+++ app/models/button.rb
@@ -1,4 +1,4 @@
 class Button < ApplicationRecord
   # @Question: Wie kann ich auf die content_elements ein dependent: :destroy aufrufen?
-  has_many :sections, as: :content_element, touch: true
+  has_many :sections, as: :content_element
 end
